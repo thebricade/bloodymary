acd0003a7c219bfbd63e77454ae3f7cf04f50402
Bri <bs3864@nyu.edu>
2023-02-20 22:44:30 -0500

    Worked on Layout draft

---
+++ BloodyMary/Assets/Scripts/SceneManger.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus; 
+
+public class SceneManger : MonoBehaviour
+{
+    private Flowchart _flowchart; 
+    
+    public enum GameScenes
+    {
+        HappyBirthday,
+        Mash,
+        AolIntro,
+        OuijaBoard
+    }
+
+    public GameScenes currentScene;
+    private GameScenes targetScene; 
+    private bool changeScene = false; 
+    
+   //This is a prototype script when looking to refactor let's see if there's a way to make this a statemaching
+   // or see if it's worth.
+   //might be good to run an end state that runs a specific camera fade. 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _flowchart = GameObject.Find("Flowchart").GetComponent<Flowchart>();  //see if there's a better way to do this
+        //set the first scene
+        currentScene = GameScenes.HappyBirthday; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("pressing E");
+            if (changeScene)
+            {
+                changeScene = false;
+                currentScene = targetScene; 
+                _flowchart.SendFungusMessage(targetScene.ToString());
+            }
+        }
+    }
+
+
+    public void ChangeScene(GameScenes nextScene)
+    {
+        if (nextScene == GameScenes.Mash)
+        {
+            Debug.Log("Moving to Mash Scene");
+            targetScene = GameScenes.Mash;
+            changeScene = true;
+        }
+
+        if (nextScene == GameScenes.AolIntro)
+        {
+            Debug.Log("AOL scene started");
+            targetScene = GameScenes.AolIntro;
+            changeScene = true; 
+        }
+        
+    }
+    
+
+}
