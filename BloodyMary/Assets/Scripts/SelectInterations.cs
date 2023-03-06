080c336fbacb6be6c0cd2c514048a201d0271f35
Bri <bs3864@nyu.edu>
2023-03-05 21:04:19 -0500

    Select Interaction

--- BloodyMary/Assets/Scripts/SelectInterations.cs
+++ BloodyMary/Assets/Scripts/SelectInterations.cs
@@ -4,29 +4,39 @@ using UnityEngine;
 
 public class SelectInterations : MonoBehaviour
 {
-    public Camera playerCamera; 
+    public Camera playerCamera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log(Input.mousePosition);
-        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+       Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+       RaycastHit hit;
 
-        RaycastHit hit;
+        //Debug.DrawLine(transform.position, transform.forward, Color.green);
 
         if (Physics.Raycast(ray, out hit))
         {
+            Debug.DrawRay(ray.origin, ray.direction, Color.blue);
             if (hit.collider != null)
             {
                 Debug.Log("hit something");
                 if (hit.collider.gameObject.CompareTag("KeyItem"))
                 {
                     Debug.Log("I hit cake");
+                    Debug.DrawRay(ray.origin, ray.direction * 100, Color.green, 3f);
+                    
+                }
+                else
+                {
+                  
+                
                 }
             }
         }
