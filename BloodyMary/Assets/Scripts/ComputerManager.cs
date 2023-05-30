35e033584b0acd8d84557171348fb65121fb579c
Bri <bs3864@nyu.edu>
2023-05-29 22:47:05 -0400

    Message showing up on computer
    Need to clean up interaction system

---
+++ BloodyMary/Assets/Scripts/ComputerManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerManager : MonoBehaviour
+{
+    public ExamineItem _ExamineItem;
+    public SceneManger _SceneManger;
+
+    public GameObject computerScreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_ExamineItem.currentState == ItemState.Examine)
+        {
+            computerScreen.SetActive(true);
+            _SceneManger.ChangeScene(SceneManger.GameScenes.FirstAoLConvo);
+            _ExamineItem.currentState = ItemState.Idle; 
+        }
+    }
+}
